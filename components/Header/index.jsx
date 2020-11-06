e429fb2e5cf10de7c68f8c3a73f78a5268a14416
lnardon <lucaslnardon@gmail.com>
2020-11-06 00:01:23 -0300

    ✨ Add header back button

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -2,10 +2,19 @@ import React from "react";
 
 import styles from "../../styles/Header.module.css";
 import logo from "../../public/logo.png";
+import back from "../../public/back.svg";
 
-function Header() {
+function Header({ showBackBtn, backBtn }) {
   return (
     <div className={styles.headerContainer}>
+      {showBackBtn && (
+        <img
+          src={back}
+          alt="back"
+          onClick={backBtn}
+          className={styles.backBtn}
+        />
+      )}
       <img src={logo} alt="Logo" className={styles.logo} />
       <h1 className={styles.headerTitle}>Keyper</h1>
     </div>
