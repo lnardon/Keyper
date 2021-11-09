3996185fa1875ec197b23cf568db4aa17590df0d
Lucas Nardon <lucaslnardon@gmail.com>
2021-11-09 10:10:07 -0300

    💄 Update folder and foldercard UI

--- components/Foldercard/index.jsx
+++ components/Foldercard/index.jsx
@@ -1,12 +1,27 @@
+import { useEffect, useRef } from "react";
 import styles from "../../styles/Foldercard.module.css";
-import close from "../../public/close.svg";
+import deleteIcon from "../../public/assets/delete.png";
+import folderIcon from "../../public/assets/folder.png";
 
-function Foldercard({ title, onClick, deleteBtn }) {
+function Foldercard({ title, onClick, deleteBtn, index }) {
+  const folderCardRef = useRef();
+
+  useEffect(() => {
+    folderCardRef.current.style.animationDelay = `${index * 100}ms`;
+  }, [folderCardRef]);
+
   return (
-    <div className={styles.foldercardContainer} onClick={onClick}>
-      <h1 className={styles.foldercardTitle}>{title}</h1>
+    <div
+      ref={folderCardRef}
+      className={styles.foldercardContainer}
+      onClick={onClick}
+    >
+      <div className={styles.cardContent}>
+        <img src={folderIcon} alt="Folder Icon" className={styles.folderIcon} />
+        <h1 className={styles.foldercardTitle}>{title}</h1>
+      </div>
       <img
-        src={close}
+        src={deleteIcon}
         alt="delete"
         className={styles.deleteBtn}
         onClick={(e) => {
