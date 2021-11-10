1e109bd54e624e942c9b1238896cf2ecd7cc148c
Lucas Nardon <lucaslnardon@gmail.com>
2021-11-10 00:30:24 -0300

    💄 Update folder modal UI

--- components/AddFolderModal/index.jsx
+++ components/AddFolderModal/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+import folderIcon from "../../public/assets/folder.png";
 import styles from "../../styles/AddModal.module.css";
 
 function AddFolderModal({ createFolder }) {
@@ -7,14 +8,32 @@ function AddFolderModal({ createFolder }) {
 
   return (
     <div className={styles.addModalContainer}>
-      <h1 className={styles.title}>Create folder</h1>
-      {/* <label className={styles.inputLabel}>Folder name:</label> */}
-      <input
-        type="text"
-        onChange={(e) => setName(e.target.value)}
-        className={styles.input}
-        placeholder="Folder name"
-      />
+      <h1 className={styles.title}>Folder</h1>
+      <span className={styles.dot}></span>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          border: "2px solid #5200ff",
+          marginBottom: "1rem",
+          borderRadius: "0.25rem",
+        }}
+      >
+        <img
+          src={folderIcon}
+          alt="Folder Icon"
+          className={styles.folderIcon}
+          style={{ height: "35px" }}
+        />
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          className={styles.input}
+          placeholder="Folder name"
+        />
+      </div>
+
       <button onClick={() => createFolder(name)} className={styles.createBtn}>
         Create
       </button>
